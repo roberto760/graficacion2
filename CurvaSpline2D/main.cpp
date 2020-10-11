d8d415c8e345bec51147639cafbff5beb40781de
Mikaelo <miguel.hdz.mrn@gmail.com>
2020-10-10 20:29:08 -0500

    simon

--- CurvaSpline2D/main.cpp
+++ CurvaSpline2D/main.cpp
@@ -7,33 +7,37 @@ void spline2D(int pc[][2], int n)
 {
     float x,y,u;
     setcolor(YELLOW);
-    for(int i=0; i<n; i++)
-        circle(pc[i][0], pc[i][1],2);
+    for(int j=0; j<n; j++)
+        circle(pc[j][0], pc[j][1],2);
 
     // Curva
-    for(n=3; n<8; n++)
+    for(int i=3; i<n; i++)
     {
         for(u=0; u<1; u+=0.005)
         {
-            x= (1-u)*(1-u)*(1-u)*pc[n-3][0]/6 +
-               (3*u*u*u-6*u*u+4)*pc[n-2][0]/6  +
-               (-3*u*u*u+3*u*u+3*u+1)*pc[n-1][0]/6 +
-               u*u*u*pc[n][0]/6;
+            x= (1-u)*(1-u)*(1-u)*pc[i-3][0]/6 +
+               (3*u*u*u-6*u*u+4)*pc[i-2][0]/6  +
+               (-3*u*u*u+3*u*u+3*u+1)*pc[i-1][0]/6 +
+               u*u*u*pc[i][0]/6;
 
-            y= (1-u)*(1-u)*(1-u)*pc[n-3][1]/6 +
-               (3*u*u*u-6*u*u+4)*pc[n-2][1]/6  +
-               (-3*u*u*u+3*u*u+3*u+1)*pc[n-1][1]/6 +
-               u*u*u*pc[n][1]/6;
-            //n%15+1 Asegura un id de color entre 1 y 15
-            putpixel(x, y, n%15+1);
+            y= (1-u)*(1-u)*(1-u)*pc[i-3][1]/6 +
+               (3*u*u*u-6*u*u+4)*pc[i-2][1]/6  +
+               (-3*u*u*u+3*u*u+3*u+1)*pc[i-1][1]/6 +
+               u*u*u*pc[i][1]/6;
+            //i%15+1 Asegura un id de color entre 1 y 15
+            //i%15+1 Asegura un id de color entre 1 y 15
+            putpixel(x, y, 1);
         }
     }
 }
 
 int main()
 {
-    int curva[8][2]=
+    int curva[10][2]=
     {
+
+        {50,   80},
+        {70,   100},
         {20,   20},
         {120,  80},
         {160, 140},
@@ -48,7 +52,7 @@ int main()
 
     int gd = DETECT,gm;
     initgraph(&gd, &gm,NULL);
-    spline2D(curva,8);
+    spline2D(curva,10);
 
     /* while( !kbhit() );
     closegraph( );*/
